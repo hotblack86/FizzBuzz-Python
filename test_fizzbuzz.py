9247f58860ce42d44e567fcd8751a1450c47fad4
James Hotblack <james.c.hotblack@gmail.com>
2019-02-12 17:49:06 +0000

    Added more tests for other numbers. All passing.

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -11,4 +11,13 @@ def test_fizzbuzz3():
     assert fizzbuzz(15) == 'fizzbuzz'
 
 def test_fizzbuzz4():
-    assert fizzbuzz(1) == 1     
+    assert fizzbuzz(37) == 37
+
+def test_fizzbuzz5():
+    assert fizzbuzz(6) == 'fizz'
+
+def test_fizzbuzz6():
+    assert fizzbuzz(10) == 'buzz'
+
+def test_fizzbuzz7():
+    assert fizzbuzz(45) == 'fizzbuzz'        
