7f7e3205d0d3d670e2ee31fad50eae792cabc6e5
James Hotblack <james.c.hotblack@gmail.com>
2019-02-12 17:40:05 +0000

    Three tests passing. FizzBuzz.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,6 @@
 def fizzbuzz(x):
 
+  if x % 15 == 0: return 'fizzbuzz'
   if x % 3 == 0: return 'fizz'
   if x % 5 == 0: return 'buzz'
   
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -5,4 +5,7 @@ def test_fizzbuzz1():
     assert fizzbuzz(3) == 'fizz'
 
 def test_fizzbuzz2():
-    assert fizzbuzz(5) == 'buzz'   
+    assert fizzbuzz(5) == 'buzz' 
+
+def test_fizzbuzz3():
+    assert fizzbuzz(15) == 'fizzbuzz'  
